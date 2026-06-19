cc0e5c9bb1f3696f0ed81ec310595074b7122181
Raushan Kumar <raushtechwatson@gmail.com>
2026-06-19 15:28:28 +0530

    first step

---
+++ src/main/java/com/raushtech/WebApp/WebAppApplication.java
@@ -0,0 +1,14 @@
+package com.raushtech.WebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/raushtech/WebApp/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.raushtech.WebApp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String greet(){
+        return "Welcome to WebApp Home....";
+    }
+}
