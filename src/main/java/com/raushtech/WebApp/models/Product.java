a81519d7180914d21e8117f773c75322ab9c5758
Raushan Kumar <raushtechwatson@gmail.com>
2026-06-20 03:19:57 +0530

    third step commit

--- src/main/java/com/raushtech/WebApp/models/Product.java
+++ src/main/java/com/raushtech/WebApp/models/Product.java
@@ -3,12 +3,13 @@ package com.raushtech.WebApp.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.stereotype.Component;
+
+import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private int id;
     private String prodName;
